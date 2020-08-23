f321a6a82567708d1fc943f873b82d17dd810d4c
JulianCambraia <juliancambraia@gmail.com>
2020-08-23 20:14:42 -0300

    Segundo commit, Com Page Index Controller Thymeleaf.

---
+++ src/test/java/juliancambraia/springframework/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package juliancambraia.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
