bf2779494a2ea97f797c9a5300443cc4d6c5d782
JulianCambraia <juliancambraia@gmail.com>
2020-11-08 18:55:15 -0300

    Adiciona Test Integration Repository.

---
+++ src/test/java/juliancambraia/springframework/repositories/UnitOfMeasureRepositoryITTest.java
@@ -0,0 +1,37 @@
+package juliancambraia.springframework.repositories;
+
+import juliancambraia.springframework.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryITTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cups");
+        assertEquals("Cups", unitOfMeasure.get().getDescription());
+    }
+}
